d9feccffed74dcd588c9e9713be4faa1a29cecfd
Simachew Tibebu <stibebu@avain.intra>
2019-08-20 18:35:59 +0300

    more style

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,7 +1,9 @@
 <template>
     <div v-if="video" class="col-md-8">
         <div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" :src="videoUrl"></iframe>
+            <iframe class="embed-responsive-item" 
+            :src="videoUrl">
+            </iframe>
             </div>
         <div class="details">
             <h4>{{ video.snippet.title }}</h4>
